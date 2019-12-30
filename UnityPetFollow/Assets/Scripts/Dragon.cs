c54dc68e907ce712400cbe6934681c042d8d1e59
matt2133leo <>
2019-12-31 01:34:43 +0800

    回家作業_W8

---
+++ UnityPetFollow/Assets/Scripts/Dragon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Dragon : MonoBehaviour
+{
+
+    [Header("玩家位置")]
+    private Transform player;
+    
+
+    [Range(0f,100f)]
+    public float speed = 10f;
+
+    private void Start()
+    {
+        player = GameObject.Find("private target").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Pet_track();
+    }
+
+    /// <summary>
+    /// 龍龍追蹤方法
+    /// </summary>
+    private void Pet_track()
+    {
+        Vector3 posPet = transform.position;
+        Vector3 posplayer = player.position;
+
+        transform.position = Vector3.Lerp(posPet , posplayer, 0.05f * Time.deltaTime * speed);
+        transform.LookAt(posplayer);
+
+    }
+
+
+}
